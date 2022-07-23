1690cbc1a094293783ce28f56575a04bfefb23bc
Max Sof <maxeinsof@gmail.com>
2022-07-23 18:56:23 +0700

    implement Dependency Inversion Principle

--- src/core/router/Router.js
+++ src/core/router/Router.js
@@ -1,5 +1,6 @@
 import { $ } from '@core/dom'
 import { ActiveRoute } from '@core/router/ActiveRoute'
+import { Loader } from '@/components/Loader'
 
 export class Router {
   constructor(selector, routes) {
@@ -9,6 +10,8 @@ export class Router {
 
     this.$placeholder = $(selector)
     this.routes = routes
+
+    this.loader = new Loader()
 
     this.page = null
 
@@ -22,14 +25,17 @@ export class Router {
     this.changePageHandler()
   }
 
-  changePageHandler() {
+  async changePageHandler() {
     if (this.page) {
       this.page.destroy()
     }
 
+    this.$placeholder.clear().append(this.loader)
+
     const Page = this.routes[ActiveRoute.name] || this.routes.dashboard
     this.page = new Page(ActiveRoute.param)
-    this.$placeholder.clear().append(this.page.getRoot())
+    const root = await this.page.getRoot()
+    this.$placeholder.clear().append(root)
 
     this.page.afterRender()
   }
